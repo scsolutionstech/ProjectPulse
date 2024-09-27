5a578af14cd4c762fde5e953ed64b0f8a9b33059
BOSumitC <sumit.chawla@blueocean.systems>
2024-09-28 03:20:42 +0530

    update accounts admin file

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,11 +1,24 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
-# Register your models here.
-class CustomUserAdmin(admin.ModelAdmin):
 
-    list_display = ('username', 'email', 'role')
-    list_filter = ('role',)
-   
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ('email', 'username', 'role', 'is_staff', 'is_superuser')
+    search_fields = ('email', 'username')
+    ordering = ('email',)
+
+    fieldsets = (
+        (None, {'fields': ('username', 'email', 'password')}),
+        ('Personal Info', {'fields': ('role', 'profile_picture')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'username', 'password1', 'password2', 'role', 'profile_picture', 'is_staff', 'is_superuser')}
+        ),
+    )
 
 admin.site.register(CustomUser, CustomUserAdmin)
