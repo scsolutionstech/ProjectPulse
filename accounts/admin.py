fbd3fb8e052648609459bbd6bd198c98742614c4
BOSumitC <sumit.chawla@blueocean.systems>
2024-09-28 03:54:07 +0530

    fix bugs in admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import CustomUser
+from accounts.models import CustomUser
 
 class CustomUserAdmin(UserAdmin):
     model = CustomUser
-    list_display = ('email', 'username', 'role', 'is_staff', 'is_superuser')
+    list_display = ('email', 'username', 'role', 'is_staff', 'is_superuser',)
     search_fields = ('email', 'username')
     ordering = ('email',)
 
@@ -12,7 +12,7 @@ class CustomUserAdmin(UserAdmin):
         (None, {'fields': ('username', 'email', 'password')}),
         ('Personal Info', {'fields': ('role', 'profile_picture')}),
         ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser')}),
-        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+        ('Important dates', {'fields': ('date_joined',)}),
     )
     add_fieldsets = (
         (None, {
